58fa4f1468caa0d70d52eeefdd67a7d2a02cc685
Michael Centrelli <Centm860@newschool.edu>
2017-08-30 09:51:55 -0600

    Access to API

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -3,10 +3,9 @@ class NrelService
   attr_reader :tokken, :conn
 
   def initialize
-    @tokken = ENV["TOKKEN"]
-    @conn = Faraday.new(url: "https://api.data.gov/nrel/alt-fuel-stations/v1") do |faraday|
+    @conn = Faraday.new(url: "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1") do |faraday|
     faraday.adapter Faraday.default_adapter
-    faraday.params[:access_token] = tokken
+    faraday.headers["X-Api-Key"] = ENV["TOKKEN"]
     end
   end
 
