29c09ba97e24c574980e0ddcb059d1893b6cc74a
Michael Centrelli <Centm860@newschool.edu>
2017-08-30 09:12:31 -0600

    Test Added for Search Function

---
+++ spec/features/return_search_results_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "API Key Call" do
+  it "returns the thing" do
+    
+    visit "/"
+    fill_in "search", with: "80203" 
+    click_on "Locate"
+
+    expect(current_path).to eq("/search")
+  end
+end
